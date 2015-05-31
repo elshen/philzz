113ee4594fb3de98111f554a35762c3fb2652647
elshen <flyingcitrusfruits19@gmail.com>
2015-05-30 21:50:10 -0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+$(document).ready(function(event){
+	var ref = new Firebase("https://philz4schoolz.firebaseio.com");
+ref.authWithOAuthPopup("facebook", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+})
